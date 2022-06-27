a7bc6e629b6e54596048667c15942468aa3dfeda
guilherme-lima4 <guilherme.lima.dinizt@gmail.com>
2022-06-26 23:55:44 -0300

    Corrigindo Script telas de login e cadastro - definitivo

--- cadastroVendedor.php
+++ cadastroVendedor.php
@@ -144,21 +144,23 @@ if(isset($_POST['email']) || isset($_POST['senha']) || isset($_POST['nome']) || 
 	<div id="dropDownSelect1"></div>
 	
 <!--===============================================================================================-->
-	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+<script>window.jQuery || document.write('<script src="vendor/jquery/jquery-3.2.1.min.js"><\/script>')</script>
 <!--===============================================================================================-->
-	<script src="vendor/animsition/js/animsition.min.js"></script>
+<script src='vendor/animsition/js/animsition.min.js'></script>
 <!--===============================================================================================-->
-	<script src="vendor/bootstrap/js/popper.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src='vendor/bootstrap/js/popper.js'></script>
+<script src='vendor/bootstrap/js/bootstrap.min.js'></script>
 <!--===============================================================================================-->
-	<script src="vendor/select2/select2.min.js"></script>
+<script src='vendor/select2/select2.min.js'></script>
 <!--===============================================================================================-->
-	<script src="vendor/daterangepicker/moment.min.js"></script>
-	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<script src='vendor/daterangepicker/moment.min.js'></script>
+<script src='vendor/daterangepicker/daterangepicker.js'></script>
 <!--===============================================================================================-->
-	<script src="vendor/countdowntime/countdowntime.js"></script>
+<script src='vendor/countdowntime/countdowntime.js'></script>
 <!--===============================================================================================-->
-	<script src="js/main.js"></script>
+<script src='js/main.js'></script>
 
 </body>
 </html>
